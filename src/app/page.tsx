f4da4c7bed58b4f9ef4c89229f3588cb3c7a7d45
fedevcoding <fedevcoding@gmail.com>
2024-02-10 17:57:41 +0100

    💧little advancement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+
 export default function Home() {
- return <></>;
+ return (
+  <MaxWidthWrapper>
+   <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+    <h1>
+     Your marketplace for high-quality{" "}
+     <span className="text-blue-600">TECH TOOLS</span>
+    </h1>
+
+    <button className={buttonVariants({ variant: "link" })}>
+     Start shopping
+    </button>
+   </div>
+  </MaxWidthWrapper>
+ );
 }
