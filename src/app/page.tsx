f7e9671461d68da32c684548c478102a5337396e
fedevcoding <fedevcoding@gmail.com>
2024-02-16 18:49:25 +0100

    default navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { buttonVariants } from "@/components/ui/button";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
  return (
   <MaxWidthWrapper>
-   <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-    <h1>
-     Your marketplace for high-quality{" "}
-     <span className="text-blue-600">TECH TOOLS</span>
-    </h1>
-
-    <button className={buttonVariants({ variant: "link" })}>
-     Start shopping
-    </button>
-   </div>
+   <Navbar />
   </MaxWidthWrapper>
  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const Navbar = () => {
+ return (
+  <div className="sticky z-50 top-0 h-16">
+   <header>
+    <MaxWidthWrapper>
+     <div></div>
+    </MaxWidthWrapper>
+   </header>
+  </div>
+ );
+};
+
+export default Navbar;
