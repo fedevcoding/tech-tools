3013eb7b1f65ab866b907118329c0c4dfe810fc4
fedevcoding <fedevcoding@gmail.com>
2024-02-17 15:41:20 +0100

    setup layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
  return (
   <MaxWidthWrapper>
-   <Navbar />
+   <></>
   </MaxWidthWrapper>
  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const Navbar = () => {
  return (
-  <div className="sticky z-50 top-0 h-16">
-   <header>
+  <div className="sticky z-50 top-0 h-16 border-b-2 border-gray-700">
+   <header className="relative">
     <MaxWidthWrapper>
      <div></div>
     </MaxWidthWrapper>
