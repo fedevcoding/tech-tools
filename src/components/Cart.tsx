e4c90aeb7dc83953c225668051667268071da65a
fedevcoding <fedevcoding@gmail.com>
2024-02-18 16:47:54 +0100

    🛒 add empty cart modal

---
+++ src/components/Cart.tsx
@@ -0,0 +1,29 @@
+import { ShoppingCart } from "lucide-react";
+import {
+ Sheet,
+ SheetContent,
+ SheetHeader,
+ SheetTitle,
+ SheetTrigger,
+} from "./ui/sheet";
+import { Icons } from "./Icons";
+
+const Cart = () => {
+ return (
+  <Sheet>
+   <SheetTrigger>
+    <div className="flex items-center gap-2">
+     <Icons.cart size={32} className="cursor-pointer" />
+     <span>0</span>
+    </div>
+   </SheetTrigger>
+   <SheetContent>
+    <SheetHeader>
+     <SheetTitle>Cart (0)</SheetTitle>
+    </SheetHeader>
+   </SheetContent>
+  </Sheet>
+ );
+};
+
+export default Cart;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import { Icons } from "./Icons";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Input } from "./ui/input";
+import Cart from "./Cart";
 
 const Navbar = () => {
  return (
@@ -8,18 +10,21 @@ const Navbar = () => {
    <header className="relative">
     <MaxWidthWrapper>
      <div className="flex items-center justify-between h-20">
-      <div className="flex items-center">
+      <Link className="flex items-center" href={"/"}>
        <Icons.logo />
        <h1 className="text-2xl">TechTools</h1>
-      </div>
+      </Link>
       <div className="relative w-96">
-       <Input className="w-full" placeholder="Search for the product you want" />
+       <Input
+        className="w-full"
+        placeholder="Search for the product you want"
+       />
        <Icons.search className="absolute right-4 top-[50%] translate-y-[-50%]" />
       </div>
       <div className="flex items-center gap-12">
-       <Icons.bell size={32} />
-       <Icons.cart size={32} />
-       <Icons.profile size={32} />
+       <Icons.bell size={32} className="cursor-pointer" />
+       <Cart />
+       <Icons.profile size={32} className="cursor-pointer" />
       </div>
      </div>
     </MaxWidthWrapper>
