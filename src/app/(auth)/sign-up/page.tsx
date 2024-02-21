aba2a43c16b308d9f6e2bcf28eb518b23f8bf013
fedevcoding <fedevcoding@gmail.com>
2024-02-21 08:43:03 +0100

    📜 use forms + zod

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,13 +1,29 @@
 "use client";
 
 import { Icons } from "@/components/Icons";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+ AuthCredentialsValidator,
+ TAuthCredentialsValidator,
+} from "@/lib/validators/account-validators";
 
 const Page = () => {
+ const {
+  register,
+  handleSubmit,
+  formState: { errors },
+ } = useForm<TAuthCredentialsValidator>({
+  resolver: zodResolver(AuthCredentialsValidator),
+ });
+
+ const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {};
+
  return (
   <>
    <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
@@ -15,7 +31,7 @@ const Page = () => {
      <div className="flex flex-col items-center space-y-2 text-center">
       <Icons.logo />
 
-      <h1>Create an account</h1>
+      <h1 className="text-3xl font-semibold">Create an account</h1>
 
       <Link
        href={"/sign-in"}
@@ -26,22 +42,26 @@ const Page = () => {
      </div>
 
      <div className="grid gap-6">
-      <form action="">
-       <div className="space-y-3">
+      <form onSubmit={handleSubmit(onSubmit)}>
+       <div className="grid gap-3">
         <div className="grid gap-2 py-1">
          <Label htmlFor="email">Email</Label>
          <Input
-          className={cn({ "focus-visible:ring-red-500": true })}
+          {...register("email")}
+          className={cn({ "focus-visible:ring-red-500": errors.email })}
           placeholder="Email"
          />
         </div>
         <div className="grid gap-2 py-1">
          <Label htmlFor="password">Password</Label>
          <Input
-          className={cn({ "focus-visible:ring-red-500": true })}
+          {...register("password")}
+          type="password"
+          className={cn({ "focus-visible:ring-red-500": errors.password })}
           placeholder="Password"
          />
         </div>
+        <Button>SIGN UP</Button>
        </div>
       </form>
      </div>
---
+++ src/lib/validators/account-validators.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const AuthCredentialsValidator = z.object({
+ email: z.string().email(),
+ password: z
+  .string()
+  .min(8, { message: "Password must me at least 8 charachters" }),
+});
+export type TAuthCredentialsValidator = z.infer<
+ typeof AuthCredentialsValidator
+>;
