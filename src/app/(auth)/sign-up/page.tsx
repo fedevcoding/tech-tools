871d45122616364e72398caa3ccbf105f57b8d1c
fedevcoding <fedevcoding@gmail.com>
2024-02-21 08:23:40 +0100

    🛑 add sign up buttons

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { Icons } from "@/components/Icons";
 import { buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 const Page = () => {
@@ -12,9 +17,33 @@ const Page = () => {
 
       <h1>Create an account</h1>
 
-      <Link href={"/sign-in"} className={buttonVariants({ variant: "link" })}>
+      <Link
+       href={"/sign-in"}
+       className={buttonVariants({ variant: "link", className: " gap-1.5" })}
+      >
        Already have an account? Sign-in
       </Link>
+     </div>
+
+     <div className="grid gap-6">
+      <form action="">
+       <div className="space-y-3">
+        <div className="grid gap-2 py-1">
+         <Label htmlFor="email">Email</Label>
+         <Input
+          className={cn({ "focus-visible:ring-red-500": true })}
+          placeholder="Email"
+         />
+        </div>
+        <div className="grid gap-2 py-1">
+         <Label htmlFor="password">Password</Label>
+         <Input
+          className={cn({ "focus-visible:ring-red-500": true })}
+          placeholder="Password"
+         />
+        </div>
+       </div>
+      </form>
      </div>
     </div>
    </div>
