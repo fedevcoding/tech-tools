ce17e5bfc02241d1f1840d44b56907b5413323ab
fedevcoding <fedevcoding@gmail.com>
2024-03-19 07:31:47 +0100

    Format price util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,22 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const tailwindColors = { ...tailwindConfig.theme.extend.colors };
+
+export function formatPrice(
+ price: number | string,
+ options: {
+  currency?: "USD" | "EUR" | "GBP" | "BDT";
+  notation?: Intl.NumberFormatOptions["notation"];
+ } = {}
+) {
+ const { currency = "USD", notation = "compact" } = options;
+
+ const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+ return new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency,
+  notation,
+  maximumFractionDigits: 2,
+ }).format(numericPrice);
+}
