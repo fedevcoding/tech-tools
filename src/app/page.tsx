9d77cc89644dc2e82fe02368f151447d5ea4ee96
fedevcoding <fedevcoding@gmail.com>
2024-03-19 14:16:38 +0100

    Add product reel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 
 export default function Home() {
  return (
   <MaxWidthWrapper>
-   <></>
+   <ProductReel
+    query={{ sort: "desc", limit: 4 }}
+    title="Top sellers"
+    subtitle="Explore our most popular products."
+   />
   </MaxWidthWrapper>
  );
 }
