38c34f56ef308e0a33d8015c35447112a8e1f6dd
fedevcoding <fedevcoding@gmail.com>
2024-03-23 09:23:17 +0100

    add left sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import ProductReel from "@/components/ProductReel";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
  return (
   <MaxWidthWrapper>
-   <ProductReel
-    query={{ sort: "desc", limit: 18 }}
-    title="Top sellers"
-    subtitle="Explore our most popular products."
-   />
+   <div className="flex">
+    <Sidebar />
+    <ProductReel
+     query={{ sort: "desc", limit: 18 }}
+     title="Top sellers"
+     subtitle="Explore our most popular products."
+    />
+   </div>
   </MaxWidthWrapper>
  );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+const Sidebar = () => {
+ return (
+  <div className="w-[15%] h-[calc(100vh-5rem)] flex flex-col justify-around border-r-defaultGray border-r-2">
+   <h1 className="text-lg font-bold">Categories</h1>
+   <a>Somethign</a>
+   <a>Somethign</a>
+   <a>Somethign</a>
+   <a>Somethign</a>
+   <a>Somethign</a>
+   <a>Somethign</a>
+   <a>Somethign</a>
+   <a>Somethign</a>
+  </div>
+ );
+};
+
+export default Sidebar;
